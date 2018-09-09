dd595db56489ed8978b10ca110b00476dbd80e32
Paul G-P <paulgp@gmail.com>
2018-09-09 14:54:22 -0400

    Transitioned Code to tidycensus

--- code/clean-data.R
+++ code/clean-data.R
@@ -2,47 +2,85 @@
 library(tidyverse)
 library(stringr)
 library(ggthemes)
+library(tidycensus)
+library(readxl)
+library(viridis)
+## key = Your Census API key. Obtain one at http://api.census.gov/data/key_signup.html
+key = "6dc01cb16b173a97a1215ea441de09bc0d2da25d"
 
-col_names <- names(read_csv('../raw-data/ACS_16_5YR_B02001_with_ann.csv', n_max = 0 ))
-var_names <- read_csv('../raw-data/ACS_16_5YR_B02001_with_ann.csv', n_max = 1 )
-data <- read_csv('../raw-data/ACS_16_5YR_B02001_with_ann.csv', col_names = col_names, skip = 2)
+acs_varnames  <- load_variables(2016, "acs5", cache = TRUE)
 
-
-
-clean_data2 <- data %>% select(-contains('02_')) %>% gather(variable, value, starts_with("HD"), -GEO.id, -GEO.id2, -`GEO.display-label`) %>%
-    select(-GEO.id, -GEO.id2) 
- 
-
-var_data <- var_names %>% select(-GEO.id, -GEO.id2, -`GEO.display-label`, -contains('02_')) %>%
-    gather(variable, varname ) %>%
-        mutate(varname = str_replace(varname, 'Estimate; ' , '')) %>%
-            mutate(varname = str_replace(varname, ':' , '')) %>%
-                mutate(varname = str_replace(varname, '- ' , ''))
-
-
-clean_data <- inner_join(clean_data2, var_data, by='variable') %>% rename(location = `GEO.display-label`) %>% select(-variable)
-clean_data <- clean_data %>% mutate(location = case_when(
-                          location == "Pennsylvania" ~ "state",
-                          location == "Philadelphia County, Pennsylvania" ~ "county",
-                          TRUE ~ 'block_group')) 
-
-
-totals <- clean_data %>% filter(varname == "Total") %>% rename(total = value) %>%
-    select(-varname)
-
-race_share <- clean_data %>% inner_join(totals, by='location') %>%
-    mutate(frac = value / total) %>%
-    select(-value, -total)    %>%
-        spread(location,frac)  %>% filter(varname != "Total")  %>%
-            mutate(varname = str_replace(varname, 'Total ', '')) %>%
-                filter(!grepl('Two or more races: - Two races excluding', varname)) %>%
-                    filter(!grepl('Two or more races: - Two races including', varname)) %>%
-                        gather(location, value, c(block_group, county, state), -varname)
+pull_vars <- c("B01001_002",
+               "B01001_026",
+               "B01002_001",
+               "B01002A_001",
+               "B01002B_001",
+               "B01002C_001",
+               "B01002D_001",
+               "B01002E_001",
+               "B01002F_001",
+               "B01002G_001",
+               "B01002H_001",
+               "B01002I_001",
+               "B02001_001",
+               "B02001_002",
+               "B02001_003",                              
+               "B02001_004",
+               "B02001_005",
+               "B02001_006",
+               "B02001_007",
+               "B02001_008",
+               "B02001_009",
+               "B02001_010"               
+               )
 
 
+pull_vars_varnames <- acs_varnames %>%
+    filter(name %in% pull_vars) %>%
+    rename(variable = name)
 
-ggplot(data = race_share) +
-    geom_col(aes(x = varname,y = value)) + theme_tufte() +
-        coord_flip()  + facet_grid(. ~ location)
+blockgroup_data <- get_acs(geography = "block group",
+                           variables = pull_vars,
+                           state     = "PA",
+                           county    = "Philadelphia",
+                           key       = key,
+                           cache     = TRUE
+                           )
+
+blockgroup_data <- blockgroup_data %>%
+    left_join(by="variable", pull_vars_varnames)
+
+
+CensusMapping <- read_excel("~/repos/stats-for-julia/raw-data/CensusMapping.xlsx", 
+                            col_types = c( "numeric", "text","numeric", 
+                                          "blank"), n_max = 28)
+colnames(CensusMapping) <- c("division", "GEOID", "share")
+
+
+race_data <- blockgroup_data %>%
+    right_join(by="GEOID", CensusMapping) %>%
+    filter(concept =="RACE") %>%
+    separate(col = "label",
+             into = c("A", "B", "C"),
+             sep = "!!") %>%
+    group_by(division, C) %>%
+    mutate(scale_estimate = estimate * share) %>%
+    summarize(total_scale= sum(scale_estimate),
+              total = sum(estimate)) %>%
+    rename(race = C) %>%
+    mutate(race = replace_na(race, "Total")) %>%
+    group_by(division) %>%
+    filter(race != "Total") %>%
+    mutate(all_total = sum(total), all_total_scale = sum(total_scale)) %>%
+    mutate(share_frac = total / all_total,
+           share_frac_scale= total_scale / all_total_scale) %>%
+    ungroup()
+
+g <-ggplot(data = race_data %>% filter(race != "Total") %>%
+               mutate(division = as.factor(division)))
+
+g + geom_col(aes(division,share_frac, fill=race)) +
+    theme_tufte() 
+
 
 ggsave("../output/figure_race.pdf")
